46cf62a75544d64cf215ad8a6dd30f9c72fd01be
William VanDolah <william.vandolah@gmail.com>
2018-07-10 15:39:14 -0500

    working on adv, able to move from room to room, kinda

--- src/adv.py
+++ src/adv.py
@@ -2,6 +2,7 @@
 # typing "n", "w", "s", or "e" for north, west, south, and east.
 
 # These are the existing rooms. Add more as you see fit.
+import textwrap
 
 rooms = {
     "outside": {
@@ -55,13 +56,17 @@ earlier adventurers. The only exit is to the south.""",
 """
 
 # Write a class to hold player information, e.g. what room they are in currently
+class Player:
+    def __init__(self, setRoom):
+        self.currentRoom = setRoom
+
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+newPlayer = Player("narrow")
 # Write a loop that:
 #
 # * Prints the current room name
@@ -72,3 +77,23 @@ earlier adventurers. The only exit is to the south.""",
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+playing = True
+
+while playing:
+    print("\n{}\n".format(rooms[newPlayer.currentRoom]['name']))
+
+    for l in textwrap.wrap(rooms[newPlayer.currentRoom]['description'],width=50):
+        print("{}".format(l))
+    
+    print("\n")
+
+    userControl = input("what do you want to do? \nn=north, s=south, e=east, w=west, q=quit: ")
+
+    if userControl == "q":
+        playing = False
+    elif userControl in ['e','s','n','w']:
+        print('found control')
+        newPlayer.currentRoom = rooms[newPlayer.currentRoom]["{}_to".format(userControl)]
+    else:
+        print("{} is not something this game is able to do. try again".format(userControl))
